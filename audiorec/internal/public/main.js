a6730f14a4257b0557ab6fff351bfd6ffd6b0fa8
Loh Siu Yin <siuyin@beyondbroadcast.com>
2024-04-04 11:23:27 +0000

    feat: add audio recording

---
+++ audiorec/internal/public/main.js
@@ -0,0 +1,43 @@
+const recordButton = document.getElementById('record-btn');
+let mediaRecorder;
+let audioChunks = [];
+
+recordButton.addEventListener('mousedown', startRecording);
+recordButton.addEventListener('mouseup', stopRecordingAndDownload);
+
+function startRecording() {
+    navigator.mediaDevices.getUserMedia({ audio: true })
+        .then(stream => {
+            mediaRecorder = new MediaRecorder(stream,{mimeType:"audio/webm;codecs=opus",audioBitsPerSecond:16000});
+            // mediaRecorder = new MediaRecorder(stream,{mimeType:"audio/webm;codecs=opus"});
+            mediaRecorder.ondataavailable = e => audioChunks.push(e.data);
+            mediaRecorder.start();
+        })
+        .catch(err => console.error(err));
+}
+
+function stopRecordingAndDownload() {
+    if (mediaRecorder) {
+        mediaRecorder.stop();
+        mediaRecorder.onstop = () => {
+            const blob = new Blob(audioChunks, { type: 'audio/webm' });
+            const url = window.URL.createObjectURL(blob);
+            const filename = myDateFmt(new Date()) + ".webm";
+            const link = document.createElement('a');
+            link.href = url;
+            link.download = filename;
+            link.click();
+            audioChunks = [];
+        };
+    }
+}
+
+function myDateFmt(date) {
+    return date.getFullYear()
+        + String(date.getMonth() + 1).padStart(2, "0")
+        + String(date.getDate()).padStart(2, "0")
+        + "-"
+        + String(date.getHours()).padStart(2, "0")
+        + String(date.getMinutes()).padStart(2, "0")
+        + String(date.getSeconds()).padStart(2, "0");
+}
--- videorec/internal/public/modules/greet.js
+++
@@ -1,9 +0,0 @@
-function hello(name) {
-    console.log(`Hello ${name}. Good to meet you.`)
-}
-
-function goodbye(name) {
-    console.log(`Goodbye ${name}. See you again.`)
-}
-
-export {hello, goodbye};
